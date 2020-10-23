989d4fff2e07e1381cc16ae9ba12b69f0a87f422
Nekokitty30 <72338825+Nekokitty30@users.noreply.github.com>
2020-10-23 07:16:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, chet, nechet;
+            /*scanf("a,", &n);*/
+            chet = 0; // четные
+            nechet = 0; // нечет.
+            while (n > 0)
+            {
+                if (n % 2 == 0) chet += 1;
+                else nechet += 1;
+                n = n / 10;
+            }
+            Console.WriteLine("четных - a, нечетных - b\n", chet, nechet);
+        }
+    }
+}
